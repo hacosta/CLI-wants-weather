5c586a7e67fcb7d14787cced082949b5b70b69e9
Hector Acosta <hector.acosta@gmail.com>
2012-03-29 09:23:14 -0600

    Add example on how to set your PS1
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

---
+++ PS1example.sh
@@ -0,0 +1,20 @@
+
+now()
+{
+	date +%s
+}
+
+poll_every=1800 #check the weather every 30 min, otherwise, return cached weather
+$(weather.py > ~/.weather)
+weather()
+{
+	last_run=$(stat -c %Y ~/.weather)
+	if [ $(( $last_run + $poll_every )) -lt $(now) ]; then
+		echo calling!
+		$(weather.py > ~/.weather)
+	fi
+	cat ~/.weather
+}
+
+
+PS1='$(weather) \u@\h\$ '
